c0a9e2c7d29ce50ad06631404f4dd98a5935bc59
acbewley <acbewley13@gmail.com>
2021-05-05 11:26:24 -0400

    init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,20 @@
-import React from "react";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            <Books />
-          </Route>
-          <Route exact path="/books">
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail/>
-          </Route>
-          <Route path='*'>
-            <NoMatch/>
-          </Route>
-        </Switch>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
-    </BrowserRouter>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
   );
 }
+
 
 export default App;
